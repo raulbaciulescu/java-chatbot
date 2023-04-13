43f4fcdc1f43082769ffb191b0e296408c11bc76
raulbaciulescu <raulbaciulescu@gmail.com>
2023-04-13 18:36:22 +0300

    first commit

---
+++ src/main/java/com/university/JavaChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.university;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChatbotApplication.class, args);
+	}
+
+}
